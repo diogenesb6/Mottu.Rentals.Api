523a9ee955df1aeac317eb00ddd706bfc008cdb5
diogenesb6 <diogenesb6@gmail.com>
2025-08-26 16:03:48 -0300

    Use DBContext

--- Program.cs
+++ Program.cs
@@ -1,11 +1,11 @@
 using Microsoft.EntityFrameworkCore;
+using Mottu.Rentals.Api.Data;
 using System;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Configuração do banco de dados
-//builder.Services.AddDbContext<AppDbContext>(options =>
-  //  options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+//builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 // Adicionar controllers
 builder.Services.AddControllers();
