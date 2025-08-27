f82e1ce8108a758a3bdc05d9a993531e5ac0f718
diogenesb6 <diogenesb6@gmail.com>
2025-08-27 17:39:41 -0300

    criação inicial dos services e refact dos controllers

--- Controllers/EntregadorController.cs
+++ Controllers/EntregadorController.cs
@@ -2,7 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Mottu.Rentals.Api.Data;
 using Mottu.Rentals.Api.DTO;
-using Mottu.Rentals.Api.Entities;
+using Mottu.Rentals.Api.Services;
 
 namespace Mottu.Rentals.Api.Controllers
 {
@@ -11,7 +11,7 @@ namespace Mottu.Rentals.Api.Controllers
     public class EntregadorController : ControllerBase
     {
         private readonly AppDbContext _context;
-       private readonly IEntregadorService _service;
+       private readonly RiderService _service;
 
         public EntregadorController(AppDbContext context)
         {
---
+++ Services/BikeService.cs
@@ -0,0 +1,51 @@
+﻿using Mottu.Rentals.Api.Data;
+using Mottu.Rentals.Api.Entities;
+
+namespace Mottu.Rentals.Api.Services
+{
+    public class BikeService
+    {
+        private readonly AppDbContext _context;
+
+        public BikeService(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Bike> CreateBikeAsync(BikeCreateDto dto)
+        {
+            var bike = new Bike
+            {
+                Id = Guid.NewGuid(),
+                Model = dto.Model,
+                Year = dto.Year,
+                Plate = dto.Plate
+            };
+
+            _context.Bikes.Add(bike);
+            await _context.SaveChangesAsync();
+            return bike;
+        }
+
+        public async Task<Bike?> GetBikeByIdAsync(Guid id)
+        {
+            return await _context.Bikes.FindAsync(id);
+        }
+
+        public async Task<List<Bike>> ListBikesAsync()
+        {
+            return await _context.Bikes.ToListAsync();
+        }
+
+        public async Task<bool> DeleteBikeAsync(Guid id)
+        {
+            var bike = await _context.Bikes.FindAsync(id);
+            if (bike == null) return false;
+
+            _context.Bikes.Remove(bike);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+    }
+
+}
